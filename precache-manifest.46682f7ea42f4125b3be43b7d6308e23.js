9be2265465dfc71b1ca0761b080275d2481de96c
Inshal Ayaz <inshal.ayaz15@gmail.com>
2020-03-16 11:45:58 +0500

    Updates

---
+++ precache-manifest.46682f7ea42f4125b3be43b7d6308e23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e214bef064ea7e80d1fde480ddf66ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc12c4ca2b1d1ca96522",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "141a0a58e9bf92ddee79",
+    "url": "/static/css/main.d6561315.chunk.css"
+  },
+  {
+    "revision": "fc12c4ca2b1d1ca96522",
+    "url": "/static/js/2.a46dfb62.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.a46dfb62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "141a0a58e9bf92ddee79",
+    "url": "/static/js/main.679c06c2.chunk.js"
+  },
+  {
+    "revision": "7f31f04669a7e0c04c51",
+    "url": "/static/js/runtime-main.59d46d1a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
